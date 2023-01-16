9b05eac110b02b3f0f5c66b4d899dd76f184fff8
javiervazquez06 <117735361+javiervazquez06@users.noreply.github.com>
2023-01-16 11:15:56 +0100

    Add files via upload

---
+++ ej1_listas.js
@@ -0,0 +1,37 @@
+    // Crea una lista de números pares del 2 al 20 utilizando un bucle while.
+var numeros = [];
+var i= 0;
+while(i<=20){
+    numeros.push(i);
+    i = i + 2;
+}
+console.log ("lista de numeros: "+ numeros ) 
+    // Crea una lista de números impares del 1 al 19 utilizando un bucle for.
+var numerosImpar = [];
+var index = 1;
+for (var index = 1; index <= 19; index++) {
+    if(index %2== 1){
+    numerosImpar.push(index);
+}
+}
+console.log ("lista de numerosImpar: "+ numerosImpar)
+    
+    // Crea una lista de comida.
+
+var comida =["pasta","hamburguesa","salmorejo","pisto"] 
+    
+    // Agrega un elemento al principio de la lista de comida que creaste en el ejercicio anterior.
+    
+    // Accede al segundo elemento de la lista de comida y cambia su valor por otro diferente.
+    
+    // Recorre la lista de comida utilizando un bucle while y muestra cada elemento en la consola.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva el último elemento de la lista.
+    
+    // Crea una función que reciba una lista y un elemento como parámetros y agregue el elemento a la lista si no se encuentra en ella.
+    
+    // Crea una función que reciba una lista como parámetro y devuelva una nueva lista con los elementos de la lista original en orden inverso.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la suma de todos los números de la lista.
+    
+    // Crea una función que reciba una lista de números como parámetro y devuelva la media de todos los números de la lista.
